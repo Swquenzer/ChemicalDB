2323f949faaadc7901111ffb826f63de6d2247d6
Stephen Quenzer <swquenzer@gmail.com>
2014-03-20 09:16:24 -0400

    Empty input values won't be checked against the database on the fly

--- js/scanner.js
+++ js/scanner.js
@@ -45,6 +45,9 @@ function updateLabelName(chemName) {
 function verifyNewData(field, table, value, input) {
 	//input parameter is optional: Default value does nothing, otherwise verify_new_data.php receives $_GET['input']=input
 	input = typeof input !== 'undefined' ? "&input="+input : "";
+	//if value is "", return
+	console.log(value);
+	if(value === "") return;
 	ajaxRequest("verify_new_data.php?field="+field+"&table="+table+"&value="+value+input, function() {
 		if(request.readyState == 4 && request.status == 200) {
 			var response = request.responseText;
